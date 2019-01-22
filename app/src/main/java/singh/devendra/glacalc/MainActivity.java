630efdb73623857901d3c8f059d97ee91f16ca0d
devendra.singh_ca18@gla.ac.in <devendra.singh_mca18@gla.ac.in>
2019-01-23 00:04:15 +0530

    initialize the ui elements in java

--- app/src/main/java/singh/devendra/glacalc/MainActivity.java
+++ app/src/main/java/singh/devendra/glacalc/MainActivity.java
@@ -2,12 +2,22 @@ package singh.devendra.glacalc;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText etOne, etTWO;
+    Button btnCalc;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etOne = findViewById(R.id.et_first);
+        etTWO = findViewById(R.id.et_second);
+
+        btnCalc = findViewById(R.id.btn_calc);
+
     }
 }
